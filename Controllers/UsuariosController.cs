e2727d357779f89cfbc40542f5af4418a2b02b05
sthevanbello <sthevan.alves@gmail.com>
2022-08-23 21:50:07 -0300

    Criado um End Point para relacionas Usuários com Eventos

--- Controllers/UsuariosController.cs
+++ Controllers/UsuariosController.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using System.Linq;
 using System.Net.Http.Headers;
 
 namespace ApiMaisEventos.Controllers
@@ -34,9 +35,10 @@ namespace ApiMaisEventos.Controllers
                 {
                     connection.Open();
 
-                    string script = @"INSERT INTO TB_USUARIOS (Nome, Email, Senha)
+                    string script = @"INSERT INTO TB_USUARIOS 
+                                        (Nome, Email, Senha)
                                     VALUES
-                                    (@Nome, @Email, @Senha)";
+                                        (@Nome, @Email, @Senha)";
 
                     // Execução no banco
                     using (SqlCommand cmd = new SqlCommand(script, connection))
@@ -90,7 +92,9 @@ namespace ApiMaisEventos.Controllers
                 using (SqlConnection connection = new SqlConnection(connectionString))
                 {
                     connection.Open();
-                    string script = "SELECT * FROM TB_USUARIOS";
+                    string script = @"SELECT 
+                                        * 
+                                    FROM TB_USUARIOS";
                     using (SqlCommand cmd = new SqlCommand(script, connection))
                     {
                         // Ler todos os itens da consulta com foreach e while
@@ -154,6 +158,116 @@ namespace ApiMaisEventos.Controllers
             }
 
         }
+
+        /// <summary>
+        /// Listar os participantes com eventos contidos na base de dados
+        /// </summary>
+        /// <returns>listaEventosComParticipantes</returns>
+        [HttpGet]
+        [Route("Eventos")]
+        public IActionResult GetParticipantesComEventos()
+        {
+            try
+            {
+                IList<Usuario> listausuarios = new List<Usuario>();
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string script = @"SELECT 
+	                                    U.Id AS 'Id_Usuario', 
+	                                    U.Nome AS 'Nome_Usuario', 
+	                                    U.Email AS 'Email_Usuario',
+                                        E.CategoriaId AS 'Id_Categoria', 
+	                                    C.NomeCategoria AS 'Nome_Categoria',
+                                        E.Id AS 'Id_Evento', 
+	                                    E.DataHora AS 'Data_Hora_Evento',
+	                                    E.Preco AS 'Preco_Evento',
+	                                    E.Ativo AS 'Evento_Ativo'
+                                    FROM TB_USUARIOS AS U
+                                    INNER JOIN RL_USUARIO_EVENTO AS UE ON U.Id = UE.UsuarioId
+                                    LEFT JOIN TB_EVENTOS AS E ON E.Id = UE.EventoId
+                                    INNER JOIN TB_CATEGORIAS AS C ON E.CategoriaId = C.Id";
+
+                    using (SqlCommand cmd = new SqlCommand(script, connection))
+                    {
+                        // Ler todos os itens da consulta com while
+                        cmd.CommandType = CommandType.Text;
+                        using (var result = cmd.ExecuteReader())
+                        {
+                            while (result != null && result.HasRows && result.Read())
+                            {
+                                var evento = new Evento();
+                                if (!string.IsNullOrEmpty(result["Id_Evento"].ToString()))
+                                {
+                                    evento = new Evento
+                                    {
+                                        Id = (int)result["Id_Evento"],
+                                        DataHora = Convert.ToDateTime(result["Data_Hora_Evento"]),
+                                        Ativo = (bool)result["Evento_Ativo"],
+                                        Preco = (decimal)result["Preco_Evento"],
+                                        Categoria = new Categoria
+                                        {
+                                            Id = (int)result["Id_Categoria"],
+                                            NomeCategoria = result["Nome_Categoria"].ToString()
+                                        },
+                                        
+                                    };
+                                }
+
+                                if (!listausuarios.Any(x => x.Id == (int)result["Id_Usuario"]))
+                                {
+                                    var usuario = new Usuario
+                                    {
+                                        Id = (int)result["Id_Usuario"],
+                                        Nome = result["Nome_Usuario"].ToString(),
+                                        Email = result["Email_Usuario"].ToString(),
+                                        Eventos = new List<Evento>()
+                                    };
+
+                                    if ((usuario?.Id ?? 0) > 0)
+                                    {
+                                        usuario.Eventos.Add(evento);
+                                    }
+
+                                    listausuarios.Add(usuario);
+                                }
+                                else if ((evento?.Id ?? 0) > 0)
+                                {
+                                    listausuarios.FirstOrDefault(x => x.Id == (int)result["Id_Usuario"]).Eventos.Add(evento);
+                                }
+                            }
+                        }
+
+                    }
+                }
+                return Ok(listausuarios);
+            }
+            catch (InvalidOperationException ex)
+            {
+                return StatusCode(500, new
+                {
+                    msg = "Falha na conexão",
+                    erro = ex.Message,
+                });
+            }
+            catch (SqlException ex)
+            {
+                return StatusCode(500, new
+                {
+                    msg = "Falha na sintaxe do código SQL",
+                    erro = ex.Message,
+                });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new
+                {
+                    msg = "Falha na definição do código",
+                    erro = ex.Message
+                });
+            }
+        }
         /// <summary>
         /// Altera a tupla de acordo com o id fornecido como parâmetro
         /// </summary>
@@ -172,7 +286,7 @@ namespace ApiMaisEventos.Controllers
 
                     string script = @"UPDATE TB_USUARIOS 
                                         SET Nome = @Nome, Email = @Email, Senha = @Senha 
-                                        WHERE Id = @id";
+                                    WHERE Id = @id";
 
                     // Execução no banco
                     using (SqlCommand cmd = new SqlCommand(script, connection))
